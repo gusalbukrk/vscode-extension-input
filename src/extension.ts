62b313358ecf1a948e41258cd8ecbfc99c3143d8
gusalbukrk <gusalbukrk@gmail.com>
2024-02-01 17:57:17 -0300

    file picker - `showOpenDialog`

--- src/extension.ts
+++ src/extension.ts
@@ -48,38 +48,59 @@ export function activate(context: vscode.ExtensionContext) {
 		// });
 		// qp.show();
 
-		// `window.showInputBox` is easier but `window.createInputBox` is more flexible
-		const x = await vscode.window.showInputBox({
-			title: 'Input box title',
-			prompt: 'Input box prompt',
-			placeHolder: 'Input box placeHolder',
+		// // `window.showInputBox` is easier but `window.createInputBox` is more flexible
+		// const x = await vscode.window.showInputBox({
+		// 	title: 'Input box title',
+		// 	prompt: 'Input box prompt',
+		// 	placeHolder: 'Input box placeHolder',
 			
-			// value: 'mydefaultvalue',
-			// valueSelection: [2, 9], // select a range of text in the input box
+		// 	// value: 'mydefaultvalue',
+		// 	// valueSelection: [2, 9], // select a range of text in the input box
 
-			// ignoreFocusOut: true,
-			// password: true, // show password input (i.e. hide the input)
+		// 	// ignoreFocusOut: true,
+		// 	// password: true, // show password input (i.e. hide the input)
 			
-			// must return either:
-			// - human-readable string which is presented as an error message or an InputBoxValidationMessage
-			// - undefined or null to indicate that the value is valid
-			validateInput: (input) => { // runs at every keystroke
-				console.log('validating...');
-				return /^\w+$/.test(input) ? null : 'Not a valid input';
-			},
-		});
-		console.log(x);
+		// 	// must return either:
+		// 	// - human-readable string which is presented as an error message or an InputBoxValidationMessage
+		// 	// - undefined or null to indicate that the value is valid
+		// 	validateInput: (input) => { // runs at every keystroke
+		// 		console.log('validating...');
+		// 		return /^\w+$/.test(input) ? null : 'Not a valid input';
+		// 	},
+		// });
+		// console.log(x);
 
-		const ib = vscode.window.createInputBox();
-		ib.onDidHide(() => ib.dispose());
-		ib.title = "My InputBox Title";
-		// ib.step = 1; // this number is shown between parentheses after title
-		// ib.totalSteps = 1; // this number is shown together with step;
-		ib.show();
-		ib.onDidAccept( () => {
-			console.log(ib.value);
-			ib.hide();
+		// const ib = vscode.window.createInputBox();
+		// ib.onDidHide(() => ib.dispose());
+		// ib.title = "My InputBox Title";
+		// // ib.step = 1; // this number is shown between parentheses after title
+		// // ib.totalSteps = 1; // this number is shown together with step;
+		// ib.show();
+		// ib.onDidAccept( () => {
+		// 	console.log(ib.value);
+		// 	ib.hide();
+		// });
+
+		// on Windows & Linux, a file dialog cannot be both a file selector and a folder selector
+		const uris = await vscode.window.showOpenDialog({
+			// https://code.visualstudio.com/api/references/vscode-api#OpenDialogOptions
+
+			// canSelectFolders: true,
+			// canSelectFiles: true, // already default
+			canSelectMany: true,
+			openLabel: 'Select folder',
+			title: "My Dialog Title",
+			// defaultUri: vscode.Uri.file('home/user/Downloads/'),
+
+			// limit the file types that can be selected
+			// filters: {
+			// 	"Images": ["png", "jpg", "jpeg"],
+			// 	"TypeScript": ["ts", "tsx"],
+			// 	// "Allowed filetypes": ["png", "jpg", "jpeg", "ts", "tsx"],
+			// },
 		});
+
+		console.log(uris);
 	}));
 }
 
